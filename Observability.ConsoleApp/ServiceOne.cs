22cc3769298a9c349b46cb8308164d50c5b76e3d
Ender Bakır <enderyx_123@hotmail.com>
2024-03-12 19:22:50 +0300

    Add Events In ServiceOne.cs

--- Observability.ConsoleApp/ServiceOne.cs
+++ Observability.ConsoleApp/ServiceOne.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -18,10 +19,16 @@ namespace Observability.ConsoleApp
 
             using var activity = ActivitySourceProvider.Source.StartActivity(System.Diagnostics.ActivityKind.Producer , name:"CustomMakeRequestToGoogle");
 
+            var eventTags = new ActivityTagsCollection();
+            
+            activity?.AddEvent(new("google a istek başladı", tags: eventTags));//eventler T anında görmek istediğiniz olayları tracede izlemek için eklenir.
+s:
+
             var result = await httpclient.GetAsync("https://www.google.com");
 
             var responseContent = await result.Content.ReadAsStringAsync();
-
+            eventTags.Add("google body Lenght", responseContent.Length);
+            activity?.AddEvent(new("google a istek tamamlandı", tags: eventTags));
 
             return responseContent.Length;
 
