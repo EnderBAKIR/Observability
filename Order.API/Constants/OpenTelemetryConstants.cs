18362afcfa30256136abf1c2b19d28feac82fd84
Ender Bakır <enderyx_123@hotmail.com>
2024-03-21 14:48:59 +0300

    Added OpenTelemetryConstants

---
+++ Order.API/Constants/OpenTelemetryConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.API.Controllers.Constants
+{
+    internal class OpenTelemetryConstants
+    {
+        internal const string ServiceName = "Order.API"; //"CompanyName.AppName.ComponentName" 
+        internal const string ServiceVersion = "1.0.0";
+        internal const string ActiviySourceName = "ActivitySource.Order.API";
+    }
+}
--- Order.API/Program.cs
+++ Order.API/Program.cs
@@ -6,6 +6,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOpenTelemetry().WithTracing(builder =>
+{
+    builder.
+});
+
+
 
 var app = builder.Build();
 
