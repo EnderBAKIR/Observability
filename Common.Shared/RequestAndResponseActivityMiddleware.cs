970f07b2a8bfc439fda8ec79a4a87095326ee752
Ender Bakır <enderyx_123@hotmail.com>
2024-04-01 12:46:31 +0300

    Added RequestAndResponseActivityMiddleWare

---
+++ Common.Shared/RequestAndResponseActivityMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Common.Shared
+{
+    public class RequestAndResponseActivityMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public RequestAndResponseActivityMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+
+
+
+
+
+        private async Task ActivityResponse(HttpContext context)
+        {
+
+        }
+
+        private async Task ActivityRequest(HttpContext context)
+        {
+
+        }
+
+    }
+}
