6cb4cdb279c834cb1caa47c907a37c72a54020a1
Ender Bakır <enderyx_123@hotmail.com>
2024-04-03 12:22:24 +0300

    Added Initial Migration

--- Order.API/Models/AppDbContext.cs
+++ Order.API/Models/AppDbContext.cs
@@ -7,5 +7,7 @@ namespace Order.API.Models
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+        public DbSet<OrderServices.Order> Orders { get; set; }
+        public DbSet<OrderServices.OrderItem> OrderItems { get; set; }
     }
 }
