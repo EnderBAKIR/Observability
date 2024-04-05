a7de58253709017be4099812616c59a7d817d5d4
Ender Bakır <enderyx_123@hotmail.com>
2024-04-05 13:27:51 +0300

    Added CheckAndPaymentProcess Method => Stock.API/Services

---
+++ Stock.API/Controllers/StockController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Stock.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        public async Task<IActionResult> CheckAndPaymentStart()
+    }
+}
---
+++ Stock.API/StockService.cs
@@ -0,0 +1,45 @@
+﻿using Common.Shared.DTOs;
+using System.Net;
+using System.Net.WebSockets;
+
+namespace Stock.API
+{
+    public class StockService
+    {
+        public static Dictionary<int, int> GetProductStockList()
+        {
+
+            Dictionary<int, int> productStockList = new();
+            productStockList.Add(1, 10);
+            productStockList.Add(1, 20);
+            productStockList.Add(1, 30);
+
+            return productStockList;
+        }
+
+        public async Task<ResponseDto<StockCheckAndPaymenProcessResponseDto>> CheckAndPaymentProcess(StockCheckAndPaymenProcessRequestDto request)
+        {
+            var productStockList = GetProductStockList();
+
+            var stockStatus = new List<(int productId, bool hasStockExist)>();
+
+            foreach (var orderItem in request.OrderItems)
+            {
+                var hasExistStock = productStockList.Any(x => x.Key == orderItem.ProductId && x.Value >= orderItem.Count);
+
+                stockStatus.Add((orderItem.ProductId, hasExistStock));
+
+            }
+
+            if (stockStatus.Any(x=>x.hasStockExist== false))
+            {
+                return ResponseDto<StockCheckAndPaymenProcessResponseDto>.Fail(HttpStatusCode.BadRequest.GetHashCode(), "stock yetersiz");
+            }
+            return ResponseDto<StockCheckAndPaymenProcessResponseDto>.Success(HttpStatusCode.OK.GetHashCode(), 
+                new StockCheckAndPaymenProcessResponseDto() { Description="Stock Ayrıldı"});
+           
+            // payment süreci başyalacak
+
+        }
+    }
+}
